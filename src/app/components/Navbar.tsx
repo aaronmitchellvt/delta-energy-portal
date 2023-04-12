541822573a0f11c02c93645516e43dc461a34b12
aaron mitchell <aaronmitchell9902@gmail.com>
2023-04-12 11:55:43 -0400

    test

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { Button } from "@mui/material";
+import Link from "next/link";
 
 const fetchData = async () => {
   //get the logged in user
@@ -9,7 +10,7 @@ const fetchData = async () => {
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between bg-slate-800 p-4 text-white">
-      <h1 className="text-3xl font-bold">Delta Energy</h1>
+      <Link className="text-white no-underline" href="/properties"><h1 className="text-3xl font-bold">Delta Energy</h1></Link>
       <p>aaron@email.com</p>
       <Button variant="contained">
         Logout
