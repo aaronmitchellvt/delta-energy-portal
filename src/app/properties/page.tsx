ec4d83d74326767634599ffb410ac5cf8d0f5f32
aaron mitchell <aaronmitchell9902@gmail.com>
2023-04-12 00:15:13 -0400

    vercel build 2

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -4,9 +4,7 @@ import Link from "next/link";
 export default function Properties() {
   return (
       <main className="min-h-screen flex">
-        {/* <div className="h-full"> */}
           <Sidebar />
-        {/* </div> */}
         <div className="p-3 flex items-center">
           <p className="mr-2">Select a property or</p>
           <Link href="/properties/new">create one</Link>
